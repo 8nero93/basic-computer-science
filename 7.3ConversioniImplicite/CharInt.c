953be0077dbe9f87a8964a0046dc8426095c8483
Saman <nazeriehs@gmail.com>
2020-05-19 23:37:53 +0200

    gitignore update

--- 7.3ConversioniImplicite/CharInt.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	char carattere = 'A';
-	int intero = carattere;
-	printf("Carattere %c con codice %d\n", intero, intero);
-}
--- 7.4ConversioniImplicite/IntDouble.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	int intero = 1321;
-	double reale = intero;
-	printf(" Numero reale %f", reale);
-}
